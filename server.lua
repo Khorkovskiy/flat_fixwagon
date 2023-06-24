4d4431636be967938d9292fc390388280913b83e
Khorkovskiy <108166669+Khorkovskiy@users.noreply.github.com>
2023-06-24 15:20:27 +0300

    Update server.lua
    add close menu event

--- server.lua
+++ server.lua
@@ -3,11 +3,12 @@ VORP = exports.vorp_inventory:vorp_inventoryApi()
 
 VORP.RegisterUsableItem(Config.FixItem, function(data)
 	VORP.subItem(data.source, Config.FixItem, 1)
+	VORP.CloseInv(data.source)
 	TriggerClientEvent('flat:FixWagon', data.source)
 end)
 
 RegisterNetEvent("flat:addfixitem")
 AddEventHandler("flat:addfixitem", function()
-    
+    VORP.CloseInv(source)
     VORP.addItem(source, Config.FixItem, 1)
 end)
